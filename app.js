f84a00f27a862002ff3d78499b35ecde73440f71
JimmyLin <disillusioned2077@gmail.com>
2024-02-29 22:45:29 +0800

    feat: debug about db

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const initializePassport = require('./config/passport');
 (async () => {
   if (process.env.NODE_ENV === 'production') {
     await loadSecrets()
+    console.log(process.env.RDS_HOSTNAME)
   }
 
   await initializePassport();
