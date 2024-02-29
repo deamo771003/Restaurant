8a7bb0cf02c34c9bde2d6705363bf1f81bfa3c17
JimmyLin <disillusioned2077@gmail.com>
2024-02-29 21:58:13 +0800

    feat: debug about passport

--- models/index.js
+++ models/index.js
@@ -29,12 +29,11 @@ async function initialize() {
       return file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js';
     })
     .forEach(file => {
-      const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-      console.log(`Model loaded: ${model.name}`);
+      const modelDefiner = require(path.join(__dirname, file));
+      const model = modelDefiner(sequelize, Sequelize.DataTypes);
       db[model.name] = model;
     });
 
-  console.log('Associating models...');
   Object.keys(db).forEach(modelName => {
     if (db[modelName].associate) {
       console.log(`Associating model: ${modelName}`);
