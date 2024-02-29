454842153e77de6d451dbb34e9f2784c0465877e
JimmyLin <disillusioned2077@gmail.com>
2024-02-29 21:34:30 +0800

    feat: debug about passport

--- models/index.js
+++ models/index.js
@@ -15,19 +15,14 @@ let sequelize;
 
 async function initialize() {
   if (env === 'production') {
-    await loadSecrets()
-    console.log('Secrets loaded.')
+    await loadSecrets();
+    console.log('Secrets loaded.');
   }
 
   console.log(`Using environment: ${env}`);
   console.log(`Database host is set to: ${config.host}`);
 
-  const sequelize = new Sequelize(config.database, config.username, config.password, {
-    host: config.host,
-    dialect: config.dialect,
-    port: config.port,
-    logging: env === 'production' ? console.log : false,
-  });
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
 
   fs.readdirSync(__dirname)
     .filter(file => {
@@ -37,7 +32,6 @@ async function initialize() {
       const modelPath = path.join(__dirname, file);
       const model = require(modelPath)(sequelize, Sequelize.DataTypes);
       db[model.name] = model;
-
     });
 
   Object.keys(db).forEach(modelName => {
@@ -46,16 +40,16 @@ async function initialize() {
     }
   });
 
-  await initializeDatabase(sequelize);
+  await initializeDatabase();
 }
 
-async function initializeDatabase(sequelize) {
+async function initializeDatabase() {
   try {
     await sequelize.authenticate();
     console.log('Connection has been established successfully.');
     await sequelize.sync({ force: env !== 'production' });
     console.log('All models were synchronized successfully.');
-    await runSeeders(sequelize);
+    await runSeeders();
   } catch (error) {
     console.error('Unable to connect to the database:', error);
   }
@@ -65,7 +59,6 @@ async function runSeeders() {
   const usersCount = await db.User.count();
   if (usersCount === 0) {
     console.log('Running seeders...');
-    // 假設這些是您的 Seeder 文件
     await require('../seeders/20230523031140-users-seed-file').up(sequelize.getQueryInterface(), Sequelize);
     await require('../seeders/20230525092648-categories-seed-file').up(sequelize.getQueryInterface(), Sequelize);
     await require('../seeders/20230525092649-restaurants-seed-file').up(sequelize.getQueryInterface(), Sequelize);
@@ -80,6 +73,5 @@ initialize();
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
-db.initializeDatabase = initializeDatabase;
 
 module.exports = db;
