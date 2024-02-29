e57c8da3b57684382d0b5cec24a9acf98fda1b72
JimmyLin <disillusioned2077@gmail.com>
2024-02-29 23:57:20 +0800

    feat: debug about redis

--- config/redis.js
+++ config/redis.js
@@ -6,31 +6,32 @@ const { loadSecrets } = require('../helpers/loadSecrets');
     await loadSecrets()
     console.log(process.env.REDIS_URL)
   }
-
-  const client = redis.createClient({ url: process.env.REDIS_URL })
-
-  await client.connect()
-
-  client.on('ready', () => {
-    console.log('Redis client is ready.')
-  });
-
-  client.on('connect', () => {
-    console.log('Redis client has connected to the server.')
-  });
-
-  client.on('error', (err) => {
-    console.error('Redis err:', err)
-  });
-
-  client.on('end', () => {
-    console.log('Redis connection closed.')
-  });
-
-  // Uncomment if needed
-  // client.on('reconnecting', () => {
-  //   console.log('Redis client is reconnecting.');
-  // });
-
-  module.exports = client
 })();
+
+const client = redis.createClient({ url: process.env.REDIS_URL })
+
+await client.connect()
+
+client.on('ready', () => {
+  console.log('Redis client is ready.')
+});
+
+client.on('connect', () => {
+  console.log('Redis client has connected to the server.')
+});
+
+client.on('error', (err) => {
+  console.error('Redis err:', err)
+});
+
+client.on('end', () => {
+  console.log('Redis connection closed.')
+});
+
+// Uncomment if needed
+// client.on('reconnecting', () => {
+//   console.log('Redis client is reconnecting.');
+// });
+
+module.exports = client
+
