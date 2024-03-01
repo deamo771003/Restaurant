67404494a66283f96b8d67c3e538d7d41e4736f7
JimmyLin <disillusioned2077@gmail.com>
2024-03-01 21:04:25 +0800

    feat: debug about DB initialize

--- models/index.js
+++ models/index.js
@@ -60,7 +60,6 @@ async function initializeDatabase() {
     console.log('Connection has been established successfully.')
     await sequelize.sync()
     console.log('All models were synchronized successfully.')
-    await runSeeders()
   } catch (error) {
     console.error('Unable to connect to the database:', error)
   }
