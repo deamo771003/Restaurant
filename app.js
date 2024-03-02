45447bd7d27588130c0808cd69c95a84018c7457
JimmyLin <disillusioned2077@gmail.com>
2024-03-02 16:47:11 +0800

    feat: debug about DB initialize

--- app.js
+++ app.js
@@ -19,51 +19,49 @@ const port = process.env.PORT || 3000
 const db = require('./models')
 const { loadSecrets } = require('./helpers/loadSecrets')
 
-  (async () => {
-    if (process.env.NODE_ENV == 'production') {
-      await loadSecrets()
-    }
+if (process.env.NODE_ENV == 'production') {
+  loadSecrets()
+}
 
-    await db.initializeDatabase()
-    console.log('Database initialization complete.')
+db.initializeDatabase()
+console.log('Database initialization complete.')
 
-    app.engine('hbs', handlebars({ extname: '.hbs', helpers: handlebarsHelpers }))
-    app.set('view engine', 'hbs')
+app.engine('hbs', handlebars({ extname: '.hbs', helpers: handlebarsHelpers }))
+app.set('view engine', 'hbs')
 
-    app.use(express.urlencoded({ extended: true }))
-    // let redisStore = new RedisStore({ client })
-    app.use(session({
-      // store: redisStore,
-      secret: process.env.SESSION_SECRET,
-      resave: false,
-      saveUninitialized: false,
-      cookie: {
-        maxAge: 60 * 60 * 1000 * 24,
-        secure: false,
-        httpOnly: true,
-        sameSite: 'lax'
-      }
-    }))
+app.use(express.urlencoded({ extended: true }))
+// let redisStore = new RedisStore({ client })
+app.use(session({
+  // store: redisStore,
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 60 * 60 * 1000 * 24,
+    secure: false,
+    httpOnly: true,
+    sameSite: 'lax'
+  }
+}))
 
-    app.use(flash());
-    app.use(passport.initialize())
-    app.use(passport.session())
-    app.use(methodOverride('_method'))
+app.use(flash());
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(methodOverride('_method'))
 
-    app.use('/upload', express.static(path.join(__dirname, 'upload')))
+app.use('/upload', express.static(path.join(__dirname, 'upload')))
 
-    app.use((req, res, next) => {
-      res.locals.success_messages = req.flash('success_messages')
-      res.locals.error_messages = req.flash('error_messages')
-      res.locals.user = getUser(req)
-      next()
-    });
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  res.locals.user = getUser(req)
+  next()
+});
 
-    app.use(routes);
+app.use(routes);
 
-    app.listen(port, () => {
-      console.info(`listening on port ${port}`)
-    })
-  })();
+app.listen(port, () => {
+  console.info(`listening on port ${port}`)
+})
 
 module.exports = app
