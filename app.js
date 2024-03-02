9b7dd2b7131b134331d707e4017d8d0e9a655fb7
JimmyLin <disillusioned2077@gmail.com>
2024-03-02 16:35:06 +0800

    feat: debug about DB initialize

--- app.js
+++ app.js
@@ -17,57 +17,57 @@ const passport = require('./config/passport')
 const routes = require('./routes')
 const port = process.env.PORT || 3000
 const db = require('./models')
-const { loadSecrets } = require('./helpers/loadSecrets')
+// const { loadSecrets } = require('./helpers/loadSecrets')
 
-async function startApp() {
-  if (process.env.NODE_ENV == 'production') {
-    await loadSecrets()
+// async function startApp() {
+//   if (process.env.NODE_ENV == 'production') {
+//     await loadSecrets()
+//   }
+
+db.initializeDatabase()
+console.log('Database initialization complete.')
+
+app.engine('hbs', handlebars({ extname: '.hbs', helpers: handlebarsHelpers }))
+app.set('view engine', 'hbs')
+
+app.use(express.urlencoded({ extended: true }))
+// let redisStore = new RedisStore({ client })
+app.use(session({
+  // store: redisStore,
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 60 * 60 * 1000 * 24,
+    secure: false,
+    httpOnly: true,
+    sameSite: 'lax'
   }
+}))
 
-  await db.initializeDatabase()
-  console.log('Database initialization complete.')
+app.use(flash());
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(methodOverride('_method'))
 
-  app.engine('hbs', handlebars({ extname: '.hbs', helpers: handlebarsHelpers }))
-  app.set('view engine', 'hbs')
+app.use('/upload', express.static(path.join(__dirname, 'upload')))
 
-  app.use(express.urlencoded({ extended: true }))
-  // let redisStore = new RedisStore({ client })
-  app.use(session({
-    // store: redisStore,
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      maxAge: 60 * 60 * 1000 * 24,
-      secure: false,
-      httpOnly: true,
-      sameSite: 'lax'
-    }
-  }))
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  res.locals.user = getUser(req)
+  next()
+});
 
-  app.use(flash());
-  app.use(passport.initialize())
-  app.use(passport.session())
-  app.use(methodOverride('_method'))
+app.use(routes);
 
-  app.use('/upload', express.static(path.join(__dirname, 'upload')))
-
-  app.use((req, res, next) => {
-    res.locals.success_messages = req.flash('success_messages')
-    res.locals.error_messages = req.flash('error_messages')
-    res.locals.user = getUser(req)
-    next()
-  });
-
-  app.use(routes);
-
-  app.listen(port, () => {
-    console.info(`listening on port ${port}`)
-  })
-}
-
-startApp().catch((error) => {
-  console.error("Failed to start the server:", error)
+app.listen(port, () => {
+  console.info(`listening on port ${port}`)
 })
+// }
 
-// module.exports = app
+// startApp().catch((error) => {
+//   console.error("Failed to start the server:", error)
+// })
+
+module.exports = app
