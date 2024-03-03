ac5fbe6bea061955f790a344f14e841009ff52f2
JimmyLin <disillusioned2077@gmail.com>
2024-03-03 23:10:29 +0800

    feat: create undo seeders script

---
+++ scripts/run-undo-seeders.js
@@ -0,0 +1,18 @@
+const { exec } = require('child_process')
+const { loadSecrets } = require('../helpers/loadSecrets')
+
+async function getSecret() {
+  await loadSecrets()
+  exec('npx sequelize db:seed:undo:all', (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`)
+      return
+    }
+    console.log(`stdout: ${stdout}`)
+    console.error(`stderr: ${stderr}`)
+  })
+}
+
+(async () => {
+  await getSecret()
+})();
